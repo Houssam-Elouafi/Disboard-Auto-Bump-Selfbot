a6f743386343a6b5c8a1d067b87ae004f82f3b0a
Houssam-Elouafi <148121109+Houssam-Elouafi@users.noreply.github.com>
2025-02-25 12:08:39 +0000

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-const PORT = process.env.PORT || 4000; // Use 4000 as a fallback
+const PORT = process.env.PORT || 4000; // Use Render's assigned port or fallback to 4000
 
 http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
-}).listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+}).listen(PORT, '0.0.0.0', () => {  // Explicitly bind to 0.0.0.0
+  console.log(`Server is listening on port ${PORT} and interface 0.0.0.0`);
 });
