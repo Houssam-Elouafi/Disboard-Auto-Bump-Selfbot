43755a286f06d86f7883e3b96092266189022c7c
Houssam-Elouafi <148121109+Houssam-Elouafi@users.noreply.github.com>
2025-02-26 11:15:26 +0000

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,6 @@
 var http = require('http');
 
-const PORT = process.env.PORT || 8080;
-
 http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
-}).listen(PORT, '0.0.0.0', () => {
-  console.log(Server is listening on port ${PORT} and interface 0.0.0.0);
-});
+}).listen(8080);
