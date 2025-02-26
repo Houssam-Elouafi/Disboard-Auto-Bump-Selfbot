64acd9fb5564369a7122e9e60ec075f9eb0ff47a
Houssam-Elouafi <148121109+Houssam-Elouafi@users.noreply.github.com>
2025-02-26 11:02:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
-require('./keep_alive');
-
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client();
+require('dotenv').config()
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
 
 client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`); // Fixed syntax error
+    console.log(`Logged in as ${client.user.tag}!`)
 
-    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL);
+    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL)
     
     async function bump() {
-        await channel.sendSlash('302050872383242240', 'bump');
-        console.count('Bumped!');
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
     }
 
     function loop() {
-        // Send bump message every 2-3 hours to prevent detection.
-        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000;
+        // send bump message every 2-3 hours, to prevent detection.
+        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
         setTimeout(function () {
-            bump();
-            loop();
-        }, randomNum);
+            bump()
+            loop()
+        }, randomNum)
     }
     
-    bump();
-    loop();
-});
+    bump()
+    loop()
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
